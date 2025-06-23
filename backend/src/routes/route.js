ce7d8090a751879f2669ffd97265f68c2be6abf2
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-06-23 11:10:20 +0530

    tested auth middleware

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { register, login, logout } = require("../controllers/auth");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 const userRouter = express.Router();
+// const authMiddleware = require("../middlewares/authMiddleware");
+userRouter.route("/register").post(register);
 
-userRouter.route("/auth/register").post(register);
+userRouter.route("/login").post(login);
 
-userRouter.route("/auth/login").post(login);
-
-userRouter.route("/auth/logout").get(logout);
+userRouter.route("/logout").get(authMiddleware, logout);
 
 module.exports = {
   userRouter,
